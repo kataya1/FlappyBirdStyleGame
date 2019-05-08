08b8658e260e96ce8bad2f4af2a2d105f2de80eb
kataya1 <34242491+kataya1@users.noreply.github.com>
2019-05-08 19:57:30 +0200

    added lobby and rooms

---
+++ Assets/Scripts/New Folder/addRoomsToLobby.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addRoomsToLobby : MonoBehaviour
+{
+    public GameObject RoomTemplate;
+
+    public GameObject content;
+
+    public void AddRoomClick()
+    {
+        var copy = Instantiate(RoomTemplate);
+        copy.transform.parent = content.transform;
+    }
+}
